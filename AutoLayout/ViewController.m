7b5dc03c8b8547504569a8223d38154ba41564fe
Ramaraj T <ramarajthanga.p@zanec.com>
2014-12-22 14:40:33 +0530

    Initial Commit

---
+++ AutoLayout/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AutoLayout
+//
+//  Created by Ramaraj Thanga Pandi on 22/12/14.
+//  Copyright (c) 2014 Ramaraj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
